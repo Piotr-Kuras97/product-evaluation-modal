434c1893e4e095da4e604319118799f0d38cbaa8
Piotr Kuraś <piotr.kuras.1997@gmail.com>
2023-02-22 17:34:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+// empty page on start
+
+const loading = document.querySelector(".loading")
+
+setTimeout(()=> {
+    loading.remove()
+},3000)
+
+// evalution 
+
+const numberSelect = document.querySelectorAll(".select")
+const nextStep = document.querySelector(".card__btn")
+let rating;
+let flag = true
+
+numberSelect.forEach((item) => {
+    item.addEventListener("click", () =>{
+        if (flag){
+            item.classList.add("active")
+            nextStep.classList.add("active")
+            rating = item.textContent.replaceAll(' ','');
+            flag = false
+            return flag, rating
+        } else if (item.classList.contains("active")){
+            item.classList.remove("active")
+            nextStep.classList.remove("active")
+            rating = 0;
+            flag = true
+            return flag, rating
+        }
+    })
+})
+
+// alert
+
+nextStep.addEventListener("click", () => {
+    if(nextStep.classList.contains("active") && rating > 0 && rating < 6){
+        alert("Dziękujemy, że oceniłeś produkt na: " + rating)
+    } else {
+        alert("Error")
+    }
+})
+
+
+
+
